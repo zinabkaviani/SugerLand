bb52de9b8526ce971ae97bc21b5528f898390c1a
Zinab Kaviani <152513893+zinabkaviani@users.noreply.github.com>
2025-03-28 00:41:14 +0330

    assets

--- GameCore/Assets/Scripts/PlayerController.cs
+++ GameCore/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
     [SerializeField] public float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+    //     QualitySettings.vSyncCount = 0;
+    //     Application.targetFrameRate = 10;
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + speed;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Vector2 position = (Vector2)transform.position + move * (speed * Time.deltaTime);
         transform.position = position;
     }
 }
