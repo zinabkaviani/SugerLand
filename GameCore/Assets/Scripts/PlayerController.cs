decc00d556bea6468ae82cead57c43071a89872c
Zinab Kaviani <152513893+zinabkaviani@users.noreply.github.com>
2025-03-28 03:10:20 +0330

    player controller edited

--- GameCore/Assets/Scripts/PlayerController.cs
+++ GameCore/Assets/Scripts/PlayerController.cs
@@ -1,24 +1,32 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
 
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    private Vector2 move;
     public InputAction MoveAction;
     [SerializeField] public float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    //     QualitySettings.vSyncCount = 0;
-    //     Application.targetFrameRate = 10;
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        Vector2 position = (Vector2)transform.position + move * (speed * Time.deltaTime);
-        transform.position = position;
+        move = MoveAction.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+        Vector2 position = (Vector2)rigidbody2d.position + move * (3.0f * Time.deltaTime);
+        rigidbody2d.MovePosition(position);
     }
 }
