8894de163cadf3f954c1ad7e185f77c320f9c4a2
Zinab Kaviani <152513893+zinabkaviani@users.noreply.github.com>
2025-04-06 18:02:00 +0330

    player controller

--- GameCore/Assets/Scripts/PlayerController.cs
+++ GameCore/Assets/Scripts/PlayerController.cs
@@ -7,31 +7,63 @@ using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
+    AudioSource audioSource;
+    Animator animator;
+    Vector2 moveDirection = new Vector2(1,0);
     // Variables related to player character movement
+    public InputAction talkAction;
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
     public float speed = 3.0f;
 
+    public GameObject projectilePrefab;
 
-    public int maxHealth = 5;
-    int currentHealth = 1; 
+    
+    public int maxHealth = 10;
+    int currentHealth ; 
     public int health { get { return currentHealth; }}
     
     public float timeInvincible = 2.0f;
     bool isInvincible;
     float damageCooldown;
+    
+    public static PlayerController instance;  // Singleton instance
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);  // Prevent duplicates
+        }
+    }
     void Start()
     {
+        talkAction.Enable();
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
-        //currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        currentHealth = maxHealth;
     }
  
     // Update is called once per frame
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y,0.0f))
+        {
+            moveDirection.Set(move.x, move.y);
+            moveDirection.Normalize();
+        }
+        animator.SetFloat("Look X", moveDirection.x);
+        animator.SetFloat("Look Y", moveDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
 
         if (isInvincible)
         {
@@ -41,6 +73,11 @@ public class PlayerController : MonoBehaviour
                 isInvincible = false;
             }
         }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+       
     }
 
     void FixedUpdate()
@@ -48,19 +85,40 @@ public class PlayerController : MonoBehaviour
         Vector2 position = (Vector2)rigidbody2d.position + move * (speed * Time.deltaTime);
         rigidbody2d.MovePosition(position);
     }
-    public void ChangeHealth (int amount)
+    public void ChangeHealth(int amount)
     {
         if (amount < 0)
         {
             if (isInvincible)
-            {
                 return;
-            }
+          
             isInvincible = true;
             damageCooldown = timeInvincible;
+            animator.SetTrigger("Hit");
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        UIHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
+
+        if (UIBarHandler.instance)
+        {
+            UIBarHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
+        }
+        else
+        {
+            Debug.Log("UIBarHandler instance is null! Make sure it exists in the scene.");
+        }
+    }
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(moveDirection, 300);
+
+
+        animator.SetTrigger("Launch");
+    }
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
     
 }
